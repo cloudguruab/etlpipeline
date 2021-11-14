669db3390b308fc1defb8f92d53359f7bced3b30
cloudguruab <ap.brown011@gmail.com>
2021-11-14 18:18:41 -0500

    updated source file with transformations

--- src/source.py
+++ src/source.py
@@ -38,4 +38,19 @@ df_all.dropna(inplace=True)
 df_all['opening_price'] = df_all.sort_values(by=['Time']).groupby(['ISIN', 'Date'])['StartPrice'].transform('first')
 
 #get closing price per ticker(ISIN) and day
-df_all['closing_price'] = None
+df_all['closing_price'] = df_all.sort_values(by=['Time']).groupby(['ISIN', 'Date'])['StartPrice'].transform('last')
+
+#aggregate data into new format
+df_all = df_all.groupby(['ISIN', 'Date'], as_index=False).agg(opening_price_eur=('opening_price', 'min'),
+                                                              closing_price_eur=('closing_price', 'min'),
+                                                              minimum_price_eur=('MinPrice', 'min'),
+                                                              maximum_price_eur=('MaxPrice', 'max'),
+                                                              daily_traded_volume=('TradedVolume', 'sum'))
+
+#percent change previous closing 
+df_all['prev_closing_price'] = df_all.sort_values(by=['Date']).groupby(['ISIN'])['closing_price_eur'].shift(1)
+df_all['change_prev_closing_%'] = (df_all['closing_price_eur'] - df_all['prev_closing_price']) / df_all['prev_closing_price'] * 100
+df_all.drop(column=['prev_closing_price'], inplace=True) #no longer need prev_closing_price
+df_all = df_all.round(decimals=2)
+
+print(df_all)
