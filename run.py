b70a1f1df73e87c86568f0be05c01fc450c7e287
cloudguruab <ap.brown011@gmail.com>
2021-11-16 21:45:14 -0500

    entry point to application

--- run.py
+++ run.py
@@ -1,9 +1,25 @@
 """
 entry point into application
 """
+import logging
+import logging.config
+import yaml
+from os.path import join, dirname, realpath
 
 def main():
-    pass
+    """
+    manager for running etl job
+    """
+    #parse yaml file
+    config_path = join(dirname(realpath(__file__)), 'config/conf.yml')
+    config = yaml.safe_load(open(config_path))
 
+    #configure loggging
+    log_config = config['logging']
+    logging.config.dictConfig(log_config) 
+    logger = logging.getLogger(__name__)
+    logger.info("this is a test")
+    
+        
 if __name__ == '__main__':
     main()
