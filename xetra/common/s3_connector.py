a1c1aa74456c2465ad69aef1c3345453f5d79ffe
cloudguruab <ap.brown011@gmail.com>
2021-11-17 21:14:15 -0500

    cpatch to imports used in script

--- xetra/common/s3_connector.py
+++ xetra/common/s3_connector.py
@@ -1,7 +1,9 @@
 import os
 import boto3
 import logging
-from dot_env import load_dotenv
+import pandas as pd
+from io import StringIO
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -23,11 +25,16 @@ class S3BucketConnector:
         self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
         
-    def list_files_in_prefix(self):
-        pass
+    def list_files_in_prefix(self, prefix: str):
+        files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
+        return files
     
-    def read_csv_to_df(self):
-        pass
+    def read_csv_to_df(self, key: str, encoding: str='utf-8', sep: str=','):
+        self._logger.info(f'Reading file {self.endpoint_url} {self._bucket.name} {key}')
+        csv_obj = self._bucket.Object(key=key).get().get('Body').read().decode(encoding)
+        data = StringIO(csv_obj)
+        data_frame = pd.read_csv(data, sep=sep)
+        return data_frame
     
     def write_data_to_s3(self):
         pass
